2e8b8e454a95f1aaa65c9b67ea1df2a5047755fb
Bruce <39899119+792607724@users.noreply.github.com>
2021-08-25 14:26:10 +0800

    push new branch to seevision automation

--- customer_count_sdk_test_case/customer_count_sdk_test_case.py
+++
@@ -1,8 +0,0 @@
-# coding = utf8
-import os
-
-os.path.abspath(".")
-
-
-
-
--- windows_control/uiautomation_win.py
+++ windows_control/uiautomation_win.py
@@ -1,8 +1,11 @@
 # coding = utf8
 
 import os
+import re
 import subprocess
-import time
+from time import sleep
+
+import pyautogui
 
 os.path.abspath(".")
 
@@ -15,9 +18,61 @@ import uiautomation
     3、 pyautogui模拟键盘操作，快捷键等，对于一些uiautomation搜索不到的UI控件
 """
 
-subprocess.Popen("D:\EVCapture\EVCapture.exe")
-time.sleep(2)
-ev_frame = uiautomation.WindowControl(searchDepth=1, Name="EV录屏")
-# ev_frame.SetTopmost(True)
-# ev_frame.TextControl(Name="场景编辑").Click()
-ev_frame.TextControl(searchDepth=9, Name="图片水印").Click()
+
+def openPotplayer(potplayer_path="D:\PotPlayer\PotPlayerMini64.exe"):
+    global potplayer
+    potplayer = subprocess.Popen(potplayer_path)
+    sleep(2)
+
+
+def enterDeviceSettings():
+    global potplayer_frame
+    global settings_frame
+    potplayer_frame = uiautomation.WindowControl(searchDepth=1, Name="PotPlayer")
+    pyautogui.hotkey("alt", "d")
+    settings_frame = uiautomation.WindowControl(searchDepth=2, Name="设备设置")
+
+
+def switchResolution(resolution="YUY2 960×540P 30(P 16:9)"):
+    settings_frame.ComboBoxControl(AutomationId="3008").Click()
+    sleep(1)
+    find = False
+    count = 0
+    while not find:
+        if count < 5:
+            pyautogui.scroll(500)
+        else:
+            pyautogui.scroll(-500)
+        count += 1
+        resolution_checked = settings_frame.ListItemControl(searchDepth=6, Name=resolution)
+        findResolution = str(re.findall("Rect:(.*)(\[)", str(resolution_checked))[0][0]).strip()
+        if findResolution != "(0,0,0,0)":
+            find = True
+            resolution_checked.Click()
+            break
+    sleep(1)
+    settings_frame.ButtonControl(searchDepth=3, Name="打开设备(O)").Click()
+    sleep(1)
+
+
+def getPlayerInformation():
+    pyautogui.hotkey("ctrl", "f1")
+    sleep(3)
+    player_information = uiautomation.WindowControl(searchDepth=1, Name="播放信息")
+    current_frameRate = player_information.TextControl(AutomationId="3201").GetWindowText()
+    current_bitRate = player_information.TextControl(AutomationId="3386").GetWindowText()
+    print("当前分辨率帧率为：{}".format(current_frameRate))
+    print("当前分辨率位率为：{}".format(current_bitRate))
+
+
+def closePotplayer():
+    potplayer.kill()
+
+
+if __name__ == '__main__':
+    for i in range(3):
+        openPotplayer(potplayer_path="D:\PotPlayer\PotPlayerMini64.exe")
+        enterDeviceSettings()
+        switchResolution("YUY2 960×540P 30(P 16:9)")
+        getPlayerInformation()
+        closePotplayer()
