3cc865d1eb42d514f48bf0b0b25180a313adb74e
Bruce <792607724@qq.com>
2021-08-26 11:49:46 +0800

    Add SDK test automation script

--- pet_pupil_test_case/stress_test_case.py
+++ pet_pupil_test_case/stress_test_case.py
@@ -10,6 +10,8 @@ os.path.abspath(".")
 __author__ = "CHENGUANGTAO"
 
 from airtest.core.api import *
+from PIL import Image
+import imagehash
 
 auto_setup(__file__)
 cur_time = time.strftime("%Y%m%d_%H%M%S")
@@ -44,7 +46,6 @@ def stress_test(device, poco, test_times=1000):
             sleep(2)
             poco(text="激活").wait().click()
             sleep(2)
-
             poco(text="开始采集").wait().click()
             sleep(8)
             if poco(text="OK").wait().exists():
@@ -68,6 +69,17 @@ def stress_test(device, poco, test_times=1000):
     finally:
         result_calculate(data=csv_result)
         sys.exit(0)
+
+
+
+def launchCameraTest(device, poco):
+    for i in range(100):
+        sleep(3)
+        poco(text="相机").wait().click()
+        sleep(3)
+        device.keyevent("BACK")
+        sleep(3)
+        print("This is {} times test".format(i + 1))
 
 
 def stress_webcam_test(device, poco, test_times=1000):
@@ -99,6 +111,54 @@ def stress_webcam_test(device, poco, test_times=1000):
         sys.exit(0)
 
 
+def check_image_quality(device, poco, test_times=1000):
+    i = 1
+    csv_result = []
+    try:
+        for i in range(i, test_times):
+            sleep(3)
+            poco(text="它物云门店").wait().click()
+            sleep(2)
+            poco(text=">>模拟采集").wait().click()
+            sleep(2)
+            poco(text="激活").wait().click()
+            sleep(2)
+
+            poco(text="开始采集").wait().click()
+            sleep(5)
+            if poco(text="OK").wait().exists():
+                poco(text="OK").wait().click()
+                sleep(3)
+            if poco(text="OK").wait().exists():
+                poco(text="OK").wait().click()
+                sleep(3)
+            # 截图
+            file_name = "./screenshot/{}.jpg".format(str(i))
+            snapshot(filename = file_name)
+            sleep(3)
+            normal_picture = imagehash.average_hash(Image.open("./normal_picture.jpg"))
+            screenshot_picture = imagehash.average_hash(Image.open(file_name))
+            if normal_picture == screenshot_picture:
+                test_result = "Same picture {}".format(str(i))
+            else:
+                test_result = "Different picture {}".format(str(i))
+                break
+            print("Test times is：{} -- result is {}".format(str(i), test_result))
+            csv_result.append([i, cur_time, test_result])
+            sleep(3)
+            device.keyevent("BACK")
+            sleep(2)
+            device.keyevent("BACK")
+            sleep(2)
+            device.keyevent("BACK")
+            sleep(2)
+    except Exception as ex:
+        print("Current test is happened error, please check and error code is :{}".format(str(ex)))
+    finally:
+        result_calculate(data=csv_result)
+        sys.exit(0)
+
+
 def log_process():
     subprocess.Popen("adb -s 192.168.50.109:5555 shell logcat -b all>./stress_test.log", shell=True).communicate()[0]
 
@@ -113,12 +173,19 @@ def result_calculate(data=[["1", "2", "3"], "1", "2", "3"], form_name="result.cs
 
 
 if __name__ == '__main__':
-    init_item = init_Device(serialno="192.168.50.109:5555")
+    # init_item = init_Device(serialno="192.168.50.109:5555")
+    # init_item = init_Device(serialno="192.168.50.187:5555")
+    # init_item = init_Device(serialno="192.168.50.168:5555")
+    # init_item = init_Device(serialno="192.168.50.107:5555")
+    init_item = init_Device(serialno="1f56d837")
     device = init_item[1]
     poco = init_item[0]
 
     test_pool = multiprocessing.Pool(2)
-    test_pool.apply_async(func=stress_test(device, poco, 101))
+    # test_pool.apply_async(func=stress_test(device, poco, 101))
+    # test_pool.apply_async(func=check_image_quality(device, poco, 30))
     # test_pool.apply_async(func=log_process)
+    test_pool.apply_async(func=launchCameraTest(device, poco))
+
     test_pool.close()
     test_pool.join()
