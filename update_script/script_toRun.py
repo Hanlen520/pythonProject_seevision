d507bd9a92e3f55bff87aceb2614a1801e7dd26e
Bruce <39899119+792607724@users.noreply.github.com>
2021-09-24 17:34:53 +0800

    new input, current uiautomation_win.py cannot order the standard value now, need test on real project once.

--- update_script/script_toRun.py
+++ update_script/script_toRun.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     print("OK1")
     # device = connect_device("android:///{}?cap_method=javacap&touch_method=adb".format("c59a06bf"))
     # poco = AndroidUiautomationPoco(device, use_airtest_input=True, screenshot_each_action=False)
-    b = subprocess.Popen("python ota.py update.zip", stdout=subprocess.PIPE).communicate()[0]
+    update_command_write = subprocess.Popen("python ota.py update.zip", stdout=subprocess.PIPE).communicate()[0]
     with open("update.txt", "w") as update_command_file:
-        update_command_file.write(b.decode())
+        update_command_file.write(update_command_write.decode())
     update_command = "update.txt"
-    subprocess.Popen("adb shell < {}".format(update_command), shell=True, stdout=subprocess.PIPE).communicate()[0]
+    update_command_execute = subprocess.Popen("adb shell < {}".format(update_command), shell=True, stdout=subprocess.PIPE).communicate()[0]
     print("OK2")
