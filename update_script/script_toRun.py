666cd1d11ebdb6def4d9fa963a97809764c273ad
Bruce <39899119+792607724@users.noreply.github.com>
2021-09-24 17:43:00 +0800

    new input, current uiautomation_win.py cannot order the standard value now, need test on real project once.

--- update_script/script_toRun.py
+++ update_script/script_toRun.py
@@ -7,6 +7,8 @@ os.path.abspath(".")
 
 """
     将需要测试的update.zip、ota.py包放到脚本目录下
+    先用升级到新版本，然后再push进去update.zip包，再执行如下的循环刷入当前版本的测试即可
+    手机自带的/system/bin/update_engine_client 驱动，adb shell /system/bin/update_engine_client  --reset_status
 """
 if __name__ == "__main__":
     print("OK1")
@@ -16,5 +18,6 @@ if __name__ == "__main__":
     with open("update.txt", "w") as update_command_file:
         update_command_file.write(update_command_write.decode())
     update_command = "update.txt"
-    update_command_execute = subprocess.Popen("adb shell < {}".format(update_command), shell=True, stdout=subprocess.PIPE).communicate()[0]
+    update_command_execute = \
+    subprocess.Popen("adb shell < {}".format(update_command), shell=True, stdout=subprocess.PIPE).communicate()[0]
     print("OK2")
