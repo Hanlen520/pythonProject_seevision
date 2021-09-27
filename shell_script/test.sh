b7b07c4d82aefa74f11b1471f9ed3c0383000432
Bruce <39899119+792607724@users.noreply.github.com>
2021-09-27 14:06:16 +0800

    new input, current uiautomation_win.py cannot order the standard value now, need test on real project once.

--- shell_script/test.sh
+++ shell_script/test.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 name="cgt"
+# $ 读取变量值
 echo $name
 onlyread_variable="https://www.google.com"
+# readonly 定义该变量为只读
 readonly onlyread_variable
-echo onlyread_variable
-unset onlyread_variable
 echo $onlyread_variable
+# unset 删除变量
+unset name
+echo $name
 
+your_name="runoob"
+str1="Hello,I \"$your_name\" \n"
+# echo -e 激活转义字符
+echo -e $str1
+# 拼接字符串
+echo "hello,my name is $your_name"
+# 获取字符串长度
+echo ${#your_name}
+# 提取子字符串
