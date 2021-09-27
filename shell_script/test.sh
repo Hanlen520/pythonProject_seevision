34022479ca7ded8281d83c5517e0dce29ab93dfa
Bruce <39899119+792607724@users.noreply.github.com>
2021-09-27 11:40:48 +0800

    new input, current uiautomation_win.py cannot order the standard value now, need test on real project once.

--- shell_script/test.sh
+++ shell_script/test.sh
@@ -5,6 +5,6 @@ echo $name
 onlyread_variable="https://www.google.com"
 readonly onlyread_variable
 echo onlyread_variable
-unsett onlyread_variable
+unset onlyread_variable
 echo $onlyread_variable
 
