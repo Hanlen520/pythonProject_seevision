8a820636d6934a28a51235fd3f57e33cd6b4e097
Bruce <39899119+792607724@users.noreply.github.com>
2021-09-27 14:28:07 +0800

    new input, current uiautomation_win.py cannot order the standard value now, need test on real project once.

--- shell_script/test.sh
+++ shell_script/test.sh
@@ -14,9 +14,35 @@ echo $name
 your_name="runoob"
 str1="Hello,I \"$your_name\" \n"
 # echo -e 激活转义字符
-echo -e $str1
+# shellcheck disable=SC2090
+echo -e "$str1"
 # 拼接字符串
 echo "hello,my name is $your_name"
 # 获取字符串长度
 echo ${#your_name}
 # 提取子字符串
+echo ${your_name:2:5}
+# 查找子字符串
+label_1="Wechat is the most popular communicate tool in China"
+# shellcheck disable=SC2046
+# shellcheck disable=SC2005
+# shellcheck disable=SC2003
+echo $(expr index "$label_1" a)
+# 数组 创建数组
+name_array=("CGT" "BRUCE" "CAT" "DOG" "APPLE" "BANANA")
+# 读取指定位置元素
+echo "${name_array[3]}"
+# 读取所有元素
+echo "${name_array[@]}"
+# 获取数组元素个数
+array_length=${#name_array[@]}
+echo "$array_length"
+array_length_2=${#name_array[*]}
+echo "$array_length_2"
+# 获取单个数组元素长度
+cgt_length=${#name_array[0]}
+echo "$cgt_length"
+:<<EOF
+当前上下两行组成多行注释
+EOF
+
