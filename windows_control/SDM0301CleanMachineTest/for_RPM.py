24b403a680a91698bfa4fb1e2bb636970de63275
Bruce <39899119+792607724@users.noreply.github.com>
2021-10-19 16:09:41 +0800

    Update SDM0301 clean machine rpm.json calculate script

--- windows_control/SDM0301CleanMachineTest/for_RPM.py
+++ windows_control/SDM0301CleanMachineTest/for_RPM.py
@@ -1,37 +1,24 @@
 # coding = utf8
 import os
+
 os.path.abspath(".")
-import re
 import json
 
-def math_cal():
-    string = ""
-    rpm_json = r"C:\Users\CHENGUANGTAO\Desktop\SMD0301扫地机\扫地机测试用例 (1)\扫地机测试用例 (1)\temp\rpm_20211019142426.json"
-    with open(rpm_json,"r") as file:
-        string = file.read()
-        file.close()
+"""
+    用于扫地机RPM.json文件分析，计算平均值，直接修改rpm_json：json文件存放路径即可计算
+"""
 
-    string_array = re.findall("(\d{3}),", string)
-    sum = 0
-    for item in string_array:
-        # print(item)
-        sum += int(item)
-    print("RPM总计得：{}，RPM平均值为：{}".format(str(sum), str(sum / len(string_array))))
 
 def json_cal():
-    string = ""
     rpm_json = r"C:\Users\CHENGUANGTAO\Desktop\SMD0301扫地机\扫地机测试用例 (1)\扫地机测试用例 (1)\temp\rpm_20211019142426.json"
-    with open(rpm_json,"r") as file:
+    with open(rpm_json, "r") as file:
         file_dict = json.load(file)
-        # print(file_dict)
-        array = file_dict["rpm"]  
-        sum = 0
+        array = file_dict["rpm"]
+        sum_num = 0
         for i in array:
-            sum += i
-        print("RPM总计得：{}，RPM平均值为：{}".format(str(sum), str(sum / len(array))))
-        
-        
+            sum_num += i
+        print("总共{}组数据，RPM总计得：{}，RPM平均值为：{}".format(len(array), str(sum_num), str(sum_num / len(array))))
+
 
 if __name__ == "__main__":
-    math_cal()
-    # json_cal()
+    json_cal()
