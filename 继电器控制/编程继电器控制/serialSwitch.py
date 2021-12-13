6ba3f8979efb5cf1251f43c004f2cbf37036470c
Bruce <39899119+792607724@users.noreply.github.com>
2021-12-13 18:31:47 +0800

    增加继电器功能点代码及资料

--- 继电器控制/编程继电器控制/serialSwitch.py
+++ 继电器控制/编程继电器控制/serialSwitch.py
@@ -1,32 +1,34 @@
 # coding = utf8
 
 import os
+
 os.path.abspath(".")
 
 import serial
-import time
 
 com_id = "COM22"
 
+
 class SerialSwitch(object):
-	
-	def __init__(self, com_id):
-		self.s_obj = serial.Serial(com_id, baudrate=115200)
-		
-	def switch_pin1_on(self):
-		self.s_obj.write([0xFE, 0x05, 0x00, 0x00, 0xFF, 0x00, 0x98, 0x35])
-		print(self.s_obj.read())
-	
-	def switch_pin1_off(self):
-		self.s_obj.write([0xFE, 0x05, 0x00, 0x00, 0x00, 0x00, 0xD9, 0xC5])
-		print(self.s_obj.read())
-	
-	def close(self):
-		self.s_obj.close()
-	
+
+    def __init__(self, com_id):
+        self.s_obj = serial.Serial(com_id, baudrate=115200)
+
+    def switch_pin1_on(self):
+        self.s_obj.write([0xFE, 0x05, 0x00, 0x00, 0xFF, 0x00, 0x98, 0x35])
+        print(self.s_obj.read())
+
+    def switch_pin1_off(self):
+        self.s_obj.write([0xFE, 0x05, 0x00, 0x00, 0x00, 0x00, 0xD9, 0xC5])
+        print(self.s_obj.read())
+
+    def close(self):
+        self.s_obj.close()
+
+
 if __name__ == "__main__":
-	switch_obj = SerialSwitch(com_id)
-	switch_obj.switch_pin1_on()
-	#switch_obj.switch_pin1_off()
-	# os.system("pause")
-	print("OK - test - No")
+    switch_obj = SerialSwitch(com_id)
+    switch_obj.switch_pin1_on()
+    # switch_obj.switch_pin1_off()
+    # os.system("pause")
+    print("OK - test - No")
