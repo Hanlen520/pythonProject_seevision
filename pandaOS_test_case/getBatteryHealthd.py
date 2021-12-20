dc48b865b3e30519b10dbd2dde7f01e9cc670027
Bruce <39899119+792607724@users.noreply.github.com>
2021-12-20 17:55:54 +0800

    Add Pyqt5 designer and pyuic in seevision company environment

--- pandaOS_test_case/getBatteryHealthd.py
+++ pandaOS_test_case/getBatteryHealthd.py
@@ -7,13 +7,13 @@ os.path.abspath(".")
     @Project:PycharmProjects
     @File:getBatteryHealthd.py
     @Author:十二点前要睡觉
-    @Date:2021/12/20 17:36
+    @Date:2021/12/20 17:36s
 """
 
 if __name__ == '__main__':
     i = 0
     while True:
         i += 1
+        print("第{}次dump电池充电数据情况：\n".format(str(i)))
         result = os.system("adb shell dmesg | grep healthd")
-        print("第{}次dump电池充电数据情况：\n".format(result))
         sleep(3)
