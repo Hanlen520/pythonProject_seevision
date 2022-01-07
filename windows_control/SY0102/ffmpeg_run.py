219e620a86c79bd5af6955b4252c342fd12ffe71
Bruce <39899119+792607724@users.noreply.github.com>
2022-01-07 15:45:28 +0800

    Add Pyqt5 designer and pyuic in seevision company environment

--- windows_control/SY0102/ffmpeg_run.py
+++ windows_control/SY0102/ffmpeg_run.py
@@ -4,9 +4,9 @@ import os
 
 def create_10_folder():
     # path = r"D:\音视频一体机\GM1_48M_114\testData\PART2\\"
-    path = r"D:\音视频一体机\GM1_48M_114\testData\PART1\1COLDBOOT\\"
+    path = r"D:\音视频一体机\GM1_48M_114\testData\PART2\H265\\"
     parFolder = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"]
-    for i in range(10):
+    for i in range(5):
         i += 1
         print(path + "{}".format(i))
         os.mkdir(path + "{}".format(i))
@@ -24,13 +24,13 @@ def create_10_folder():
 
 
 if __name__ == "__main__":
-    path = r"D:\temp"
-    # videoCount = 10
-    videoCount = 1
-    for i in range(videoCount):
-        i += 1
-        print(i)
-        command = r"ffmpeg -i {}\{}.mp4 -q:v 1 -f image2 {}\{}\xxx_%05d.jpg".format(path, i, path, i)
-        print(command)
-        os.system(command)
-    # create_10_folder()
+    # path = r"D:\temp"
+    # # videoCount = 10
+    # videoCount = 1
+    # for i in range(videoCount):
+    #     i += 1
+    #     print(i)
+    #     command = r"ffmpeg -i {}\{}.mp4 -q:v 1 -f image2 {}\{}\xxx_%05d.jpg".format(path, i, path, i)
+    #     print(command)
+    #     os.system(command)
+    create_10_folder()
