801b12d927a5f87ffdb7040c648821c3d437303f
Bruce <39899119+792607724@users.noreply.github.com>
2022-01-13 10:06:18 +0800

    Add Pyqt5 designer and pyuic in seevision company environment

--- windows_control/temp/streeTest_bug.py
+++ windows_control/temp/streeTest_bug.py
@@ -16,7 +16,7 @@ import uiautomation
 from time import sleep
 
 if __name__ == '__main__':
-    for i in range(10000):
+    for i in range(100):
         print(
             "=====================================第{}次Amcap MJPEG 1080P->H264 4K分辨率切换压测=====================================".format(
                 str(i)))
@@ -25,28 +25,30 @@ if __name__ == '__main__':
         sleep(3)
         option = uiautomation.MenuItemControl(searchDepth=3, Name="Options")
         option.Click()
-        sleep(1)
+        sleep(0.5)
         video_capture_pin = uiautomation.MenuItemControl(searchDepth=3, Name="Video Capture Pin...")
         video_capture_pin.Click()
-        sleep(1)
+        sleep(0.5)
         format_list = uiautomation.ComboBoxControl(AutomationId="1058", Depth=5)
         format_list.Click()
-        sleep(1)
+        sleep(0.5)
         h264 = uiautomation.ListItemControl(Depth=7, Name="H264")
         h264.Click()
-        sleep(1)
+        sleep(0.5)
         resolution_list = uiautomation.ComboBoxControl(AutomationId="1059", Depth=5)
         resolution_list.Click()
-        sleep(1)
+        sleep(0.5)
         pyautogui.scroll(3000)
         sleep(1)
         four_k = uiautomation.ListItemControl(Depth=7, Name="3840 x 2160  (default)")
         four_k.Click()
+        sleep(0.5)
         open_button = uiautomation.ButtonControl(Depth=3, Name="确定")
         open_button.Click()
-        sleep(5)
+        sleep(3)
         pyautogui.screenshot("./bugVerified/{}.jpg".format(i))
-        sleep(1)
+        sleep(0.5)
         pid_get = subprocess.Popen("tasklist | grep amcap", shell=True, stdout=subprocess.PIPE).communicate()[0]
         pid = re.findall("amcap v3.0.9.exe(.*)Console", str(pid_get))[0].strip(" ")
         os.system("taskkill /pid {}".format(pid))
+        sleep(0.5)
