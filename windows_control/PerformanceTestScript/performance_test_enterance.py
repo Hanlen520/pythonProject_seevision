ecd6b03ab29f1aa4e3a80a3699971593d8a1eda7
CHENGUANGTAO <792607724@qq.com>
2022-01-17 18:55:06 +0800

    OK

--- windows_control/PerformanceTestScript/performance_test_enterance.py
+++ windows_control/PerformanceTestScript/performance_test_enterance.py
@@ -8,3 +8,17 @@ os.path.abspath(".")
     @Author:十二点前要睡觉
     @Date:2022/1/17 18:52
 """
+
+"""
+    性能测试主入口
+"""
+
+
+class PerformanceTestEnterance:
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
