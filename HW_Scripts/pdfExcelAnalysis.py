8ee8e946a4401ec3ff33a0c448436a6df4f66d04
CHENGUANGTAO <792607724@qq.com>
2022-03-25 20:56:15 +0800

    Optimize whole receive system to better work effectiveness

--- HW_Scripts/pdfExcelAnalysis.py
+++ HW_Scripts/pdfExcelAnalysis.py
@@ -3,10 +3,20 @@
 import os
 os.path.abspath(".")
 
+import fitz
+import glob
+import pdfplumber
 
-def readPDF(pdf_file):
-    pass
+def readPDF(pdf_file="./Mac os极简用法及设置.pdf"):
+    with pdfplumber.open(pdf_file) as pdf:
+        pdf_pages = pdf.pages
+        for i in range(0, len(pdf_pages)):
+            cur_page = pdf_pages[i]
+            print(cur_page.extract_text())
+
+
 
 
 if __name__ == '__main__':
-    pass    
+    pdf_file="./Mac os极简用法及设置.pdf"
+    readPDF(pdf_file)
