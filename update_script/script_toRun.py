c7e0ca6ae7489c76fb2d5f3204cca3caf3cb7b06
CHENGUANGTAO <792607724@qq.com>
2022-04-11 16:08:29 +0800

    Add 专利 Folder

--- update_script/script_toRun.py
+++ update_script/script_toRun.py
@@ -2,7 +2,6 @@
 
 import os
 import subprocess
-import sys
 from time import sleep
 
 os.path.abspath(".")
@@ -26,11 +25,13 @@ def check_adb_online():
     device_on = False
     while not device_on:
         print("-------------------------------------------Check device online?")
+        toTxt("-------------------------------------------Check device online?")
         sleep(5)
         adb_connnect = len(str(subprocess.Popen("adb devices", stdout=subprocess.PIPE).communicate()[0]))
         if adb_connnect > 35:
             if get_current_page():
                 print("-------------------------------------------Yes, device online……")
+                toTxt("-------------------------------------------Yes, device online……")
                 sleep(5)
                 return True
 
@@ -39,34 +40,16 @@ def root():
     if check_adb_online():
         sleep(3)
         print("-------------------------------------------Begin adb root……")
+        toTxt("-------------------------------------------Begin adb root……")
         root_step_1 = subprocess.Popen("adb root", shell=True,
                                        stdout=subprocess.PIPE).communicate()[0]
-        """
-            该部分会导致无法重复升级，故去掉
-        """
-        # sleep(3)
-        # print("-------------------------------------------Begin adb disable-verity……")
-        # root_step_2 = subprocess.Popen("adb disable-verity", shell=True,
-        #                                stdout=subprocess.PIPE).communicate()[0]
-        # sleep(3)
-        # print("-------------------------------------------Begin adb reboot……")
-        # root_step_3 = subprocess.Popen("adb reboot", shell=True,
-        #                                stdout=subprocess.PIPE).communicate()[0]
-        # sleep(3)
-        # if wait_for_device_reboot():
-        #     print("-------------------------------------------Begin adb root……")
-        #     root_step_4 = subprocess.Popen("adb root", shell=True,
-        #                                    stdout=subprocess.PIPE).communicate()[0]
-        #     sleep(3)
-        #     print("-------------------------------------------Begin adb remount……")
-        #     root_step_5 = subprocess.Popen("adb remount", shell=True,
-        #                                    stdout=subprocess.PIPE).communicate()[0]
 
 
 def get_current_page():
     current_page = subprocess.Popen("adb shell dumpsys window | grep mCurrentFocus", shell=True,
                                     stdout=subprocess.PIPE).communicate()[0]
-    if "StatusBar" in str(current_page) or "launcher3" in str(current_page):
+    if "StatusBar" in str(current_page) or "launcher3" in str(current_page) or "com.youdao.hardware.panda" in str(
+            current_page):
         return True
 
 
@@ -77,140 +60,130 @@ def wait_for_device_reboot():
             sleep(5)
             i += 1
             print("-------------------------------------------Wait for device boot!")
+            toTxt("-------------------------------------------Wait for device boot!")
             if get_current_page():
                 print("-------------------------------------------Device online!")
+                toTxt("-------------------------------------------Device online!")
             return True
 
 
-def push_updateZip_to():
+def push_updateZip_to(version_path):
     if check_adb_online():
-        print("-------------------------------------------Begin push:adb push update.zip /data/ota_package/")
+        print("-------------------------------------------Begin push:adb push {}update.zip /data/ota_package/".format(
+            version_path))
+        toTxt("-------------------------------------------Begin push:adb push {}update.zip /data/ota_package/".format(
+            version_path))
         push_into_device = \
-            subprocess.Popen("adb push update.zip /data/ota_package/", stdout=subprocess.PIPE).communicate()[
+            subprocess.Popen("adb push {}update.zip /data/ota_package/".format(version_path),
+                             stdout=subprocess.PIPE).communicate()[
                 0]
-
-
-def update_to_new_version(old_version, new_version):
-    if check_adb_online():
-        print("-------------------------------------------Analysis update.zip package……")
-        update_command_write = subprocess.Popen("python ota.py update.zip", stdout=subprocess.PIPE).communicate()[0]
-        with open("update.txt", "w") as update_command_file:
-            print("-------------------------------------------Get update code and write in update.txt……")
-            update_command_file.write(update_command_write.decode())
-            update_command_file.close()
-        update_command = "update.txt"
-        print("-------------------------------------------Execute update process……")
-        update_command_execute = \
-            subprocess.Popen("adb shell < {}".format(update_command), shell=True,
-                             stdout=subprocess.PIPE).communicate()[0]
-        sleep(10)
-        print("-------------------------------------------After update ,reboot deivce……")
-        reboot_device = subprocess.Popen("adb reboot", shell=True, stdout=subprocess.PIPE).communicate()[
-            0]
-        if check_current_version(old_version, new_version):
-            print(
-                "-------------------------------------------First version upgrade success ,Begin new version cycle flash test!")
-        else:
-            print(
-                "-------------------------------------------Please check, new version upgrade fail, cannot continue test,please check……")
 
 
 def check_current_version(old_version, new_version):
     if wait_for_device_reboot():
         print("-------------------------------------------Check current version……")
+        toTxt("-------------------------------------------Check current version……")
         current_version = (
             subprocess.Popen("adb shell getprop ro.fota.version", shell=True, stdout=subprocess.PIPE).communicate()[
                 0]).decode().strip()
         print("-------------------------------------------Current Version is {}".format(current_version))
-        if current_version == new_version:
+        toTxt("-------------------------------------------Current Version is {}".format(current_version))
+        if current_version == old_version:
             print("-------------------------------------------New version update success!")
+            toTxt("-------------------------------------------New version update success!")
             return True
-        elif current_version == old_version:
+        else:
             print("-------------------------------------------New version update fail!")
+            toTxt("-------------------------------------------New version update fail!")
             return False
 
 
 # 开始循环升级测试 --reset_status
-def begin_upgrade_test(old_version, new_version):
+def begin_upgrade_test(old_version, new_version, version_path):
     if check_adb_online():
         if get_current_page():
             print("-------------------------------------------Analysis update.zip package……")
-            update_command_write = \
-                subprocess.Popen("python ota.py update.zip", stdout=subprocess.PIPE).communicate()[0]
-            with open("update_cycle.txt", "w") as update_command_cycle_file:
-                print(
-                    "-------------------------------------------Get update cycle code and write in update_cycle.txt……")
-                update_command_cycle_file.write("update_engine_client  --reset_status=true\r\n")
-                update_command_cycle_file.write(update_command_write.decode())
-                update_command_cycle_file.write(update_command_write.decode())
-                update_command_cycle_file.close()
-            update_command = "update_cycle.txt"
+            toTxt("-------------------------------------------Analysis update.zip package……")
+            update_command = "{}ota_command.txt".format(version_path)
             print("-------------------------------------------Execute upgrade cycle update process……")
+            toTxt("-------------------------------------------Execute upgrade cycle update process……")
             update_command_execute = \
                 subprocess.Popen("adb shell < {}".format(update_command), shell=True,
                                  stdout=subprocess.PIPE).communicate()[0]
             sleep(10)
             print("-------------------------------------------After update ,reboot device……")
+            toTxt("-------------------------------------------After update ,reboot device……")
             reboot_device = subprocess.Popen("adb reboot", shell=True, stdout=subprocess.PIPE).communicate()[
                 0]
             if check_current_version(old_version, new_version):
                 print(
-                    "-------------------------------------------First version upgrade success ,Begin new version cycle flash test!")
+                    "【结果检查点：当前升级结果为：PASS】")
+                toTxt("【结果检查点：当前升级结果为：PASS】")
             else:
                 print(
-                    "-------------------------------------------Please check, new version upgrade fail, cannot continue test,please check……")
+                    "【结果检查点：当前升级结果为：FAIL】")
+                toTxt("【结果检查点：当前升级结果为：FAIL】")
 
 
-class Logger(object):
-    def __init__(self, logFile="Default.log"):
-        self.terminal = sys.stdout
-        self.log = open(logFile, 'w')
-
-    def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)
-
-    def flush(self):
-        pass
+def toTxt(result):
+    """
+    结果写入函数：将每次的结果追加写入各自串口的结果文本中
+    :param result:每次刷机后版本判断结果
+    :return:None
+    """
+    try:
+        with open("./【{}】Result.txt".format("RunningLog"), "a+") as f:
+            f.write(result + "\n")
+    except (AttributeError, TypeError) as ex:
+        print("【Error need check, maybe not important】 : \r\n{}\r\n".format(str(ex)))
+        f.write("【Error need check, maybe not important】 : \r\n{}\r\n".format(str(ex)))
 
 
 if __name__ == "__main__":
 
     # 测试前，Pycharm设置log输出方式：Run->Edit Configurations……->Logs->Save console output to file:设置自己保存的输出的文件log信息，每次测试测完更新log文件即可
 
-    sys.stdout = Logger("running.log")
+    # sys.stdout = Logger("running.log")
     """ test area: 方式1，通过reset status进行重复当前版本升降级"""
-    old_version = "EM_TK1032_20210924_v1.1.0_20210924-1315"
-    new_version = "EM_TK1032_20210924_v1.1.0_20210924-1315"
+
+    old_version = "YDC015_0.0.2_20220407_20220407-1543"
+    new_version = "YDC015_0.2.2_20220410_20220410-0055"
+    oldVersionPath = "./20220407_144459/firmware/"
+    # newVersionPath = "./20220409_234335/firmware/"
+    newVersionPath = "./20220407_144459/firmware/"
+
     print(
         "Start test and wait for device……\n We will update from\n: old - 【{}】 to new -  【{}】".format(
             old_version,
             new_version))
-    # device = connect_device("android:///{}?cap_method=javacap&touch_method=adb".format("c59a06bf"))
-    # poco = AndroidUiautomationPoco(device, use_airtest_input=True, screenshot_each_action=False)
-    root()
-    push_updateZip_to()
-    update_to_new_version(old_version, new_version)
-    for i in range(101):
+    toTxt("Start test and wait for device……\n We will update from\n: old - 【{}】 to new -  【{}】".format(
+        old_version,
+        new_version))
+    # 修改测试次数
+    for i in range(3):
         test_number = str(i + 1)
         print("-------------------------------------------Begin {} times reset cycle version upgrade test……".format(
             test_number))
+        toTxt("-------------------------------------------Begin {} times reset cycle version upgrade test……".format(
+            test_number))
         root()
-        push_updateZip_to()
-        begin_upgrade_test(old_version, new_version)
+        # version_path = ""
+        version_path = oldVersionPath
+        # if check_current_version(old_version, new_version):
+        #     # new version->old
+        #     print("Downgrade version test begin")
+        #     version_path = oldVersionPath
+        # else:
+        #     # old version->new
+        #     print("Upgrade version test begin")
+        #     version_path = newVersionPath
+        push_updateZip_to(version_path)
+        begin_upgrade_test(old_version, new_version, version_path)
         print("-------------------------------------------The {} times reset cycle version upgrade test done!".format(
             test_number))
+        toTxt("-------------------------------------------The {} times reset cycle version upgrade test done!".format(
+            test_number))
+        reboot_device = subprocess.Popen("adb reboot", shell=True, stdout=subprocess.PIPE).communicate()[
+            0]
     print("-------------------------------------------End test……")
-    # """ test area: 方式2，通过升级后通过QFIL重新刷入旧版本再进行重复当前版本升降级"""暂时不做，使用方式一根据规律判断测试结果
-    # old_version = ""
-    # new_version = "EM_TK1032_20210924_v1.1.0_20210924-1315"
-    # print(
-    #     "Start test and wait for device……\n We will update from\n: old - 【{}】 to new -  【{}】".format(
-    #         old_version,
-    #         new_version))
-    # root()
-    # push_updateZip_to()
-    # update_to_new_version(old_version, new_version)
-    # downgrade_to_old_version()
-    # print("-------------------------------------------End test……")
-    # sleep(5)
+    toTxt("-------------------------------------------End test……")
