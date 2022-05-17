102690205f4de32b23023b3c8aa1ada7e3cb75ae
chenguangtao <chenguangtao@seevision.cn>
2022-05-17 08:52:56 +0800

    Last ways to smaller the data and now optimize PADS Layout search by manually and we can auto search and scratch the element and fill into excel form check by user.

---
+++ Others/FSPWechatUpdateScript/stressTestUpdeteFSP.py
@@ -0,0 +1,12 @@
+# coding = utf8
+import os
+
+os.path.abspath(".")
+"""
+    @Project:pythonProject_seevision
+    @File:stressTestUpdeteFSP.py
+    @Author:十二点前要睡觉
+    @Date:2022/5/16 17:03
+"""
+
+
