4080359718f11af29b67a574b53f7931a647ef29
chenguangtao <chenguangtao@seevision.cn>
2022-05-18 10:10:47 +0800

    VI Bus Data Record

--- Others/VICurrentRead/VICurrentKeepReading.py
+++ Others/VICurrentRead/VICurrentKeepReading.py
@@ -1,6 +1,8 @@
 # coding = utf8
 import os
 
+import pandas as pd
+
 os.path.abspath(".")
 """
     @Project:pythonProject_seevision
@@ -8,3 +10,39 @@ os.path.abspath(".")
     @Author:十二点前要睡觉
     @Date:2022/5/17 8:47
 """
+import pyautogui
+
+pyautogui.FAILSAFE = True
+from uiautomation import uiautomation as ui
+from time import sleep
+
+
+def getVIbus(testCount):
+    data = [["number", "VBUS", "IBUS"]]
+    for i in range(1, testCount + 1):
+        sleep(0.3)
+        childrenList = ui.GroupControl(searchDepth=5, Name="参数显示").GetChildren()
+        vbus = childrenList[0].GetChildren()[1].Name
+        ibus = childrenList[1].GetChildren()[1].Name
+        print("current vbus is [{}], ibus is [{}]".format(vbus, ibus))
+        data.append([i, vbus, ibus])
+        write_into_excel("VIbusRecord.xlsx", data)
+
+
+def write_into_excel(filename, data):
+    if not os.path.exists("./result"):
+        os.mkdir("./result")
+        print("Create folder success!")
+    file_path = "./result/{}".format(filename)
+    df = pd.DataFrame(columns=data)
+    df.to_excel(file_path, index=True)
+    print("{} data fill success!".format(filename))
+
+
+if __name__ == '__main__':
+    """
+        每切换一个分辨率后，录制1000组对应的V\IBus的数据
+    """
+    testCount = 10
+    getVIbus(testCount)
+
