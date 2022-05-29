cce52f9b62345b15f43c42fa0e70b4c9c74c104c
chenguangtao <chenguangtao@seevision.cn>
2022-05-29 19:35:11 +0800

    VI Bus Data Record

--- Others/PandasStressTest/OralCheckTest.py
+++ Others/PandasStressTest/OralCheckTest.py
@@ -1,14 +1,12 @@
 # coding = utf8
 import multiprocessing
-import os
 import re
 import subprocess
 
 from airtest.core.api import *
-from airtest.core.error import DeviceConnectionError
 from poco.drivers.android.uiautomation import AndroidUiautomationPoco
-from poco.exceptions import PocoNoSuchNodeException
 
+cur_time = time.strftime("%Y%m%d_%H%M%S")
 os.path.abspath(".")
 """
     @Project:pythonProject_seevision
@@ -26,7 +24,7 @@ def get_serial_number():
     return serial_no
 
 
-def connectDevice(device_serial):
+def init_device(device_serial):
     device_ = connect_device("Android:///{}".format(device_serial[0]))
     device_.wake()
     device_.unlock()
@@ -34,23 +32,46 @@ def connectDevice(device_serial):
     return device_, poco
 
 
-def reopenTest():
-    device_ready = connectDevice(get_serial_number())
-    global device, poco
-    device = device_ready[0]
-    poco = device_ready[1]
+def reopenTest(device_, poco, i):
+    # device_ready = connectDevice(get_serial_number())
+    # global device, poco
+    # device = device_ready[0]
+    # poco = device_ready[1]
     if not os.path.exists("./snapshot/"):
         os.mkdir("./snapshot/")
-    device.home()
+    device_.home()
     position1 = poco(text="口算检查").wait(3).get_position()
     poco(text="口算检查").wait(3).click()
     position2 = poco("com.youdao.hardware.panda.countinginspect:id/ivBack").wait(3).get_position()
     poco("com.youdao.hardware.panda.countinginspect:id/ivBack").wait(3).click()
-    for i in range(10000):
-        poco.click(position1)
-        device.snapshot("./snapshot/Current{}_open.jpeg".format(i))
-        poco.click(position2)
+    try:
+        for i in range(10000):
+            poco.click(position1)
+            device_.snapshot("./snapshot/Current{}_open.jpeg".format(i))
+            poco.click(position2)
+    except Exception:
+        pass
+    finally:
+        logProcess()
+
+
+def logProcess():
+    if not os.path.exists("./log/"):
+        os.mkdir("./log/")
+    subprocess.Popen("adb logcat -b all>./log/{}_logcat.log".format(cur_time), shell=True).communicate()[0]
+
+
+def auto_case_test(device_, poco):
+    test_pool = multiprocessing.Pool(2)
+    # test_pool.apply_async(func=logProcess)
+    # 这里func改成需要测试的case方法名即可
+    # test_pool.apply_async(func=camera_operate(device_, poco, 1000))
+    test_pool.apply_async(func=reopenTest(device_, poco, 1))
+    # test_pool.apply_async(func=device_reboot(device_, poco, 1000))
+    test_pool.close()
+    test_pool.join()
 
 
 if __name__ == '__main__':
-    reopenTest()
+    device_ready = init_device(get_serial_number())
+    auto_case_test(device_ready[0], device_ready[1])
